380d2fea36a50795767fd535de87d994c54c0e6c
eros francesco reale <eros.francesco.francesco@gmail.com>
2026-04-25 11:35:02 +0000

    counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'preact/hooks';
+import './Counter.css';
 
 export function Counter() {
-	const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-	return (
-		<div class="counter">
-			<p>Counter: {count}</p>
-			<div class="counter-buttons">
-				<button onClick={() => setCount(count - 1)}>-</button>
-				<button onClick={() => setCount(count + 1)}>+</button>
-			</div>
-		</div>
-	);
+    return (
+        <div class="counter">
+            <button onClick={() => setCount(count - 1)}>-</button>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
+    );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,11 @@
-import { useLocation } from 'preact-iso';
+// import { useLocation } from 'preact-iso';
 
 export function Header() {
-	const { url } = useLocation();
+	// 	const { url } = useLocation();
 
 	return (
 		<header>
 			<nav>
-				<a href="/" class={url == '/' && 'active'}>
-					Home
-				</a>
-				<a href="/404" class={url == '/404' && 'active'}>
-					404
-				</a>
 			</nav>
 		</header>
 	);
